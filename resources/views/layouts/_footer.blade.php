f3034b1074d918659424f90908ac67f3a5923a0d
vagrant <vagrant@homestead>
2022-09-29 07:15:53 +0000

    样式调整

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,7 @@
 <footer class="footer">
   <div class="container">
-      <a href="" target="_blank">智雅（南京）科技发展有限公司</a>
+    <p class="text-center">
+      <a href="{{ url('/') }}" target="_blank" class="text-decoration-none">智雅（南京）科技发展有限公司</a>
+    </p>
   </div>
 </footer>
