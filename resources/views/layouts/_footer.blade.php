0bd965a8c7afa6175e1aec608b15585e404863e0
vagrant <vagrant@homestead>
2022-09-29 07:03:12 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <div class="container">
+      <a href="" target="_blank">智雅（南京）科技发展有限公司</a>
+  </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', 'oa企业erp员工信息管理系统-手机微信h5小程序商城制作开发-哈尔滨做手机网站-智雅开发公司') - 智雅（南京）科技发展有限公司</title>
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+      @include('shared._messages')
+
+      @yield('content')
+
+    </div>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
